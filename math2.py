3adb92831839cc94dc1760c856c174cf3465beb4
affan50pro <sweet.baby12325@gmail.com>
2024-11-13 20:12:35 +0400

    first commit

---
+++ math2.py
@@ -0,0 +1,3 @@
+num=int(input("Enter your number🙄 :"))
+for a in range(1,11,1):
+    print(num,"x",a,"=",num*a)
---
+++ math3.py
@@ -0,0 +1,5 @@
+num=int(input("Enter your number🙄 :"))
+sum=0
+for a in range(1,num+1,1):
+    sum=sum+a
+print("you anwer is =",sum)
